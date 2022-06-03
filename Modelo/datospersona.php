52d7bbb42dd3f240913fe1cf894f3e168a02d982
AleVillalb4 <avillalba793@gmail.com>
2022-06-03 20:40:48 -0300

    clasearreglada

--- Modelo/datospersona.php
+++ Modelo/datospersona.php
@@ -1,16 +1,17 @@
 <?php
 
-class DatosPersona{
+class DatosPersona
+{
     public $Nombre;
     public $Apellido;
     public $Email;
     public $Edad;
-}
 
-function MostrarPropiedades() {
-    echo 'Nombre: '.$this->Nombre.'<br>';
-    echo 'Apellido: '.$this->Apellido.'<br>';
-    echo 'Email: '.$this->Email.'<br>';
-    echo 'Edad: '.$this->Edad.'<br>';
+    function MostrarPropiedades()
+    {
+        echo 'Nombre: ' . $this->Nombre . '<br>';
+        echo 'Apellido: ' . $this->Apellido . '<br>';
+        echo 'Email: ' . $this->Email . '<br>';
+        echo 'Edad: ' . $this->Edad . '<br>';
     }
-    
+}
